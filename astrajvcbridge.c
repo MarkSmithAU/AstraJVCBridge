cd1be803e07a7bf41b044582c1dad69c60ba299d
Mark Smith <36433516+MarkSmithAU@users.noreply.github.com>
2018-07-08 15:10:40 +1000

    As-built and tested (working) in vehicle.

--- astrajvcbridge.c
+++ astrajvcbridge.c
@@ -7,32 +7,55 @@ Not licensed for commercial use
 /*
 Holden Astra to JVC Radio steering wheel remote code for ATTINY85.
 
-One resistor (470 ohm) for the voltage divider and one capacitor are the only external components required.
-The whole circuit can be powered from the 5v antenna line on the back of the JVC which has power when the unit is on.
-The internal 8Mhz oscillator is fast and accurate enough for our purposes.
+This code can be used on any vehicle that uses a 5v (or lower) output from the vehicle or a resistor divider bridge
+to supply the steering wheel controls through to the radio.  
+The ATTINY85 ADC pin must not receive a voltage above VCC (5v) i.e. 12V would be bad!  Use a high impedance resistor 
+divider to get the correct voltage if required.
 
+One resistor (470 ohm) for the voltage divider and one capacitor are the only external components required for the Astra.
+The whole circuit can be powered from the 5v amplifier line on the back of the JVC which has power when the unit is on.
+
+I/O and connection notes:
 PORTB0 is the line to connect to the JVC remote control input.
-PORTB4 is the analogue input via the 470 resistor and car i.e. 5v - 470 - car remote wire in radio harness (Blue/Red)
+PORTB4 is the analogue input via the 470 resistor and car
+	i.e. 5v -> 470 ohm resistor -> to PORTB4 AND car remote wire in radio harness (Blue/Red)
+RESET: short to VCC
+VCC: 10uF Capacitor across VCC/GND, power with a 7805 regulator from the Amplifier control wire in the JVC connector.
+GND: connect to GND in radio wiring harness
+
+The internal 8Mhz oscillator is fast and accurate enough for our purposes.
+Fuses for programming the ATTINY85:
+SELFPRGEN = [ ]
+RSTDISBL = [ ]
+DWEN = [ ]
+SPIEN = [X]
+WDTON = [ ]
+EESAVE = [ ]
+BODLEVEL = 4V3
+CKDIV8 = [ ]
+CKOUT = [ ]
+SUT_CKSEL = INTRCOSC_8MHZ_6CK_14CK_64MS
+EXTENDED = 0xFF (valid)
+HIGH = 0xDC (valid)
+LOW = 0xE2 (valid)
 
 The processing steps are:
 1. Read ADC values
-2. Translate ADC values using  tolerances to a command
-3. Debounce the command for 10ms
-4. Process the debounced command in a state machine in main() to allow sequential of single codes
+2. Translate ADC values using tolerances to a command
+3. Debounce the command for 5ms (done in the ISR to ensure consistent timing)
+4. Process the debounced command in a state machine in main() to allow sequenced codes and delays as required
 
 See https://www.avforums.com/threads/jvc-stalk-adapter-diy.248455/ for the raw protocol details.
 
-Astra raw resistance values and mappings with 5v source and 470 ohm resistor:
+Astra raw resistance values and mappings with 5v source and 458 ohm (measured) resistor:
 Button	Resistance	JVC Mapping	JVC Code	ADC Value
-None	3652		Idle		None		907
-up		1466		Source		0x08		775
-back	790			Back		0x13		642
-fwd		466			Forwards	0x12		510
-O/0		283			Mute		0x06		385
--		190			VolDn		0x05		295
-+		163			VolUp		0x04		264
-
-A tolerance of 15 ADC steps is the largest we can use due to the -/+ voltages being so close.
+None	3652		Idle		None		910
+up		1466		Source		0x08		780
+back	790			Back		0x13		648
+fwd		466			Forwards	0x12		516
+O/0		283			Sound		0x06		391
+-		83			VolDn		0x05		157
++		163			VolUp		0x04		269
 */
 
 #include <avr/io.h>
@@ -46,18 +69,18 @@ A tolerance of 15 ADC steps is the largest we can use due to the -/+ voltages be
 #define false (0)
 
 // Voltage values, assuming vref = 5v
-#define CAR_IDLE	907
-#define CAR_MUTE	385
-#define CAR_VOLDN	295
-#define CAR_VOLUP	264
-#define CAR_UPARR	775
-#define CAR_BKARR	642
-#define CAR_FDARR	510
+#define CAR_IDLE	910
+#define CAR_SOUND	391
+#define CAR_VOLDN	157
+#define CAR_VOLUP	269
+#define CAR_UPARR	780
+#define CAR_BKARR	648
+#define CAR_FDARR	516
 
 // JVC Commands
 #define JVC_VOLUP	0x04
 #define JVC_VOLDN	0x05
-#define	JVC_MUTE	0x06
+#define	JVC_SOUND	0x0D
 #define JVC_SRC		0x08
 #define JVC_SKIPBK	0x11
 #define JVC_SKIPFD	0x12
@@ -65,14 +88,14 @@ A tolerance of 15 ADC steps is the largest we can use due to the -/+ voltages be
 #define JVC_SKIPFDH 0x14
 
 // States
-enum {VAL_IDLE, VAL_VOLUP, VAL_VOLDN, VAL_SRC, VAL_SEEKFWD, VAL_SEEKBK, VAL_MUTE};
+enum {VAL_IDLE, VAL_VOLUP, VAL_VOLDN, VAL_SRC, VAL_SEEKFWD, VAL_SEEKBK, VAL_SOUND};
 
 // Calcs for ADC thresholds for handling values
-// vref = 5v
+// Vref = 5v
 // 10 bits = 1024
 // 1 bit = ~0.005V
-// +-0.2V = 30
-#define TOLLERANCE	15
+// +-0.1V = 15
+#define TOLLERANCE	30
 
 void JVCCommand(unsigned char cmd);
 void ADCInit();
@@ -105,13 +128,6 @@ ISR(TIMER1_COMPA_vect)
 	cCombined = getDebounced(&cDebounce, decodedValue);
 }
 
-void interruptSleep(unsigned long ms)
-{
-	unsigned long target;
-	target = uptime + ms;
-	while (uptime <= target);
-}
-
 int main(void)
 {
 	uint16_t adcVal;
@@ -122,8 +138,8 @@ int main(void)
 	DDRB =  0b00000000;
 	PORTB = 0b00000000; //1 for pullup
 	
-	// 10ms debounce, idle value is high, one shot is disabled (keep reporting the triggered value repeatedly)
-	initDebounce(&cDebounce, 10, VAL_IDLE, 0);
+	// 5ms debounce, idle value is high, one shot is disabled (keep reporting the triggered value repeatedly)
+	initDebounce(&cDebounce, 5, VAL_IDLE, 0);
 
 	// Set up Timer1 for 527us interrupts
 	OCR1A = (F_CPU / 4 / (128UL * (527UL - 67UL))) - 1;
@@ -144,7 +160,8 @@ int main(void)
 			adcVal = ADCRead();
 			decodedValue = DecodeAnalogue(adcVal);
 			
-			// see ISR for this as well
+			// see ISR for this as well - this only retrieves the value; it does not update it
+			tick = 0;
 			cli();
 			cCombined = getDebounced(&cDebounce, decodedValue);
 			sei();
@@ -158,7 +175,7 @@ int main(void)
 						JVCCommand(JVC_SKIPFD);
 					} else {
 						/* held */
-						JVCCommand(JVC_SKIPFDH);
+						JVCCommand(JVC_SKIPFD); /* KD-X351BT: this needs to be the original code repeated */
 					}
 					break;
 					
@@ -169,7 +186,7 @@ int main(void)
 						JVCCommand(JVC_SKIPBK);
 						} else {
 						/* held */
-						JVCCommand(JVC_SKIPBKH);
+						JVCCommand(JVC_SKIPBKH); /* KD-X351BT: this needs to be the alternate code */
 					}
 					break;
 
@@ -191,11 +208,11 @@ int main(void)
 					}
 					break;
 					
-				case VAL_MUTE:
+				case VAL_SOUND:
 					/* Only send a code once per button press */
 					if (cCombined != cCombinedLast) {
 						/* first time */
-						JVCCommand(JVC_MUTE);
+						JVCCommand(JVC_SOUND);
 					}
 					break;
 					
@@ -204,7 +221,6 @@ int main(void)
 					break;
 			}
 			cCombinedLast = cCombined;
-			tick = 0;
 		}
 	}
 
@@ -337,6 +353,6 @@ uint8_t DecodeAnalogue(uint16_t adcVal)
 	if (inRange(adcVal, CAR_UPARR, TOLLERANCE)) return VAL_SRC;
 	if (inRange(adcVal, CAR_FDARR, TOLLERANCE)) return VAL_SEEKFWD;
 	if (inRange(adcVal, CAR_BKARR, TOLLERANCE)) return VAL_SEEKBK;
-	if (inRange(adcVal, CAR_MUTE, TOLLERANCE)) return VAL_MUTE;
+	if (inRange(adcVal, CAR_SOUND, TOLLERANCE)) return VAL_SOUND;
 	return VAL_IDLE;	
 }
